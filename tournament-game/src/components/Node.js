ac1d08d9798b45b1001afd74702b77525df6c623
Thomas Mylott <mylottiv@gmail.com>
2019-12-29 17:29:45 -0600

    Background class now depends upon the state of the given node, selectNode set as the onClick for the node button

--- tournament-game/src/components/Node.js
+++ tournament-game/src/components/Node.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 function Node(props) {
-    const {content, index, parent} = props.payload;
+    const {content, index, parent, childNodes, sibling, selected, loser, primed} = props.payload;
+    const [leftChild, rightChild] = childNodes;
     return (
-        <div className='col card text-center' id={'node-' + index} parent={'node-' + parent}>
-            {content}
+        <div
+            className={'col card text-center' + ((selected) ? ' bg-success' : '') + ((loser) ? ' bg-danger' : '')  + ((primed && (!selected && !loser)) ? ' bg-warning' : '')}
+            id={'node-' + index}
+            parent={'node-' + parent}
+            left-child={'node-' + leftChild}
+            right-child={'node-' + rightChild}
+            sibling={(sibling !== null) ? 'node-' + sibling : 'none'}
+        >
+            <button className='btn-light' onClick={() => props.selectNode(index, sibling, parent)} type='button'>{content}</button>
         </div>
     )
 }
