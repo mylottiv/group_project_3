b631b246fbaa763dd95b70906873d9352cc946ba
Thomas Mylott <mylottiv@gmail.com>
2020-01-06 18:21:53 -0600

    Encapsulated the DebugToolbar as new Component

--- tournament-game/src/App.js
+++ tournament-game/src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';  
-import {Bracket, BracketProvider} from './components';
+import {Bracket, BracketProvider, DebugToolbar} from './components';
 import {Switch, Route, Link} from 'react-router-dom';
 
 function App(props) {
 
   return (
     <div className='container'>
-      <div className='row'>
-        <h3 className='btn-group-lg' role='group' aria-label='Basic example'>Choose Bracket Size (# of rounds):
-          <a className='btn btn-primary ml-1 mr-1' href="/1"><h4>1</h4></a>
-          <a className='btn btn-primary ml-1 mr-1' href="/2"><h4>2</h4></a>
-          <a className='btn btn-primary ml-1 mr-1' href="/3"><h4>3</h4></a>
-          <a className='btn btn-primary ml-1 mr-1' href="/4"><h4>4</h4></a>
-          <a className='btn btn-primary ml-1 mr-1' href="/5"><h4>5</h4></a>
-        </h3>
-      </div>
+      <DebugToolbar />
       <Switch>
         <Route path='/:number' 
           render={(props) => {
--- tournament-game/src/components/index.js
+++ tournament-game/src/components/index.js
@@ -2,5 +2,6 @@
 export {default as Bracket} from './Bracket';
 export {default as Node} from './Node';
 export {default as Round} from './Round';
+export {default as DebugToolbar} from './DebugToolbar';
 export {BracketContext} from './BracketContext';
 export {BracketProvider} from './BracketContext';
