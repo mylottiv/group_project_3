6f2dbfe9d6ffc3b326bf6d9cb115b7a49d46bada
Thomas Mylott <mylottiv@gmail.com>
2020-01-11 17:09:58 -0600

    In process of merging Tournament and Authorization

--- tournament-app/src/components/Tournament.js
+++ tournament-app/src/components/Tournament.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import '../App.css';
+import React from 'react';  
+import {Bracket, TourneyProvider, DebugToolbar} from '../components';
+import {Switch, Route, Link} from 'react-router-dom';
 
-function Tournament() {
+function Tournament(props) {
+
   return (
-    <div className="App">
-      <h1>Tournament</h1>
+    <div className='container'>
+      <DebugToolbar />
+      {/* <Switch>
+        <Route path='/:number' 
+          render={(props) => {
+            return (
+            <div className='row' id='tournament-container'>
+              <TourneyProvider {...props}>
+                  <Bracket />
+              </TourneyProvider>
+            </div>
+            )
+          }}
+         />
+      </Switch> */}
     </div>
-  );
-}
+  )
+};
 
 export default Tournament;
